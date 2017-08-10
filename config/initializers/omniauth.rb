d9bd1b000d36fb5b4b8bd87754d5f75a7aab129f
Loi Tran <lvt15@my.fsu.edu>
2017-08-11 01:56:51 +0700

    Added hover.css and worked on front end

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '1060827510719146', 'e28e885730106c47252265690c5a75e7'
+    
 end
