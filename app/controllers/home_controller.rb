d9d51b061e2b1bb32b4d1d061c6dad8446c5b35e
Loi Tran <lvt15@my.fsu.edu>
2017-08-13 18:09:22 +0700

    Corrected bug with comment deleteing for unauthorized users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :require_login
+  
   def index
     if params[:search]
       @posts = Post.search(params[:search]).order("created_at DESC")
