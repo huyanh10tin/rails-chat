cfbcca3c9fa4a406c2561e6efd96dd952514a9b0
Loi Tran <lvt15@my.fsu.edu>
2017-08-13 18:46:27 +0700

    Adding like logic for the form

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+  def create
+    current_user.likes.build(like_params)
+    current_user.save!
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def like_params
+    params.require(:like).permit(:post_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :friendships
   resources :posts
   resources :comments
+  resources :likes
 
   resources :users do
     member do
