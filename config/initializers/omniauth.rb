4e4b93142f031d276004fd50f94157f83012408f
Loi Tran <lvt15@my.fsu.edu>
2017-08-25 01:07:39 +0700

    Added the Facebook login button and removed gem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,4 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '1060827510719146', 'e28e885730106c47252265690c5a75e7'
-    
 end
